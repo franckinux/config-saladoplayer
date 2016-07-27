4e6ac37d5f39f3c7aea4532759abcddbc8f3b622
franckinux <contact@franck-barbenoire.fr>
2016-07-27 22:01:46 +0200

    add some examples

---
+++ example/pano1/config.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+from model import Panorama, Photo, Tour
+from model import GalleryHotspot, InformationHotspot, PanoramaHotspot
+from model import PhotoHotspot
+
+# --- photos
+
+photo_1_1 = Photo(
+    title="Photo 1",
+    path="media/photo-1-1.jpg"
+)
+
+photo_2_1 = Photo(
+    title="Photo 1",
+    path="media/photo-2-1.jpg"
+)
+
+photo_2_2 = Photo(
+    title="Photo 2",
+    path="media/photo-2-2.jpg"
+)
+
+photo_nadir = Photo(
+    title="Nadir photo",
+    path="media/nadir.png"
+)
+
+# --- photo hotspots
+
+photo_hotspot_1_1 = PhotoHotspot(
+    photo=photo_1_1,
+    pan=11.1,
+    tilt=22.2
+)
+
+# --- gallery hotspots
+
+gallery_hotspot_2_1 = GalleryHotspot(
+    title="Gallery",
+    pan=23,
+    tilt=57,
+    photos=[photo_2_1, photo_2_2]
+)
+
+# --- information hotspot
+
+information_hotspot_1_1 = InformationHotspot(
+    title="This is an information hotspot",
+    pan=11,
+    tilt=23
+)
+
+# --- panoramas
+
+panorama_1 = Panorama(
+    directory="tiles_1",
+    title="Panorama 1",
+    information_hotspots=[information_hotspot_1_1],
+    photo_hotspots=[photo_hotspot_1_1]
+)
+
+panorama_2 = Panorama(
+    directory="tiles_2",
+    title="Panorama 2",
+    gallery_hotspots=[gallery_hotspot_2_1],
+)
+
+# --- panorama hotspots
+
+panorama_hotspot_1_2 = PanoramaHotspot(panorama=panorama_2, pan=30, tilt=5, show_information=True)
+panorama_hotspot_2_1 = PanoramaHotspot(panorama=panorama_1, pan=25, tilt=3, show_information=False)
+
+panorama_1.panorama_hotspots.append(panorama_hotspot_1_2)
+panorama_2.panorama_hotspots.append(panorama_hotspot_2_1)
+
+# --- tour
+
+tour = Tour(
+    title="Tour exemple",
+    first_panorama=panorama_2,
+    panoramas=[panorama_1, panorama_2],
+    nadir=photo_nadir
+)
---
+++ example/pano2/config.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+from model import Panorama, Photo, Tour
+from model import InformationHotspot, PanoramaHotspot
+from model import PhotoHotspot, LinkHotspot
+
+# --- photos
+
+photo_3_1 = Photo(
+    title="Photo 1",
+    path="media/photo1.jpg"
+)
+
+photo_nadir = Photo(
+    title="Nadir photo",
+    path="media/nadir.png"
+)
+
+# --- photo hotspots
+
+photo_hotspot_3_1 = PhotoHotspot(
+    photo=photo_3_1,
+    pan=11.1,
+    tilt=22.2
+)
+
+# --- information hotspot
+
+information_hotspot_2_1 = InformationHotspot(
+    title="This is an information hotspot",
+    pan=11,
+    tilt=23
+)
+
+# --- link hotspot
+
+link_hotspot_1_1 = LinkHotspot(
+    title="This is a link hotspot",
+    pan=13,
+    tilt=27,
+    url="http://www.johndoe.com"
+)
+
+# --- panoramas
+
+panorama_1 = Panorama(
+    directory="repertoire_1",
+    title="Panorama 1"
+)
+
+panorama_2 = Panorama(
+    directory="repertoire_2",
+    title="Panorama 2",
+    information_hotspots=[information_hotspot_2_1]
+)
+
+panorama_3 = Panorama(
+    directory="repertoire_3",
+    title="Panorama 3",
+    photo_hotspots=[photo_hotspot_3_1],
+)
+
+# --- panorama hotspots
+
+panorama_hotspot_1_2 = PanoramaHotspot(panorama=panorama_2, pan=30, tilt=5, show_information=True)
+panorama_hotspot_1_3 = PanoramaHotspot(panorama=panorama_3, pan=57, tilt=11, show_information=True)
+panorama_hotspot_2_3 = PanoramaHotspot(panorama=panorama_3, pan=25, tilt=3, show_information=False)
+panorama_hotspot_3_1 = PanoramaHotspot(panorama=panorama_1, pan=13, tilt=9, show_information=True)
+
+panorama_1.panorama_hotspots.extend([panorama_hotspot_1_2, panorama_hotspot_1_3])
+panorama_2.panorama_hotspots.append(panorama_hotspot_2_3)
+panorama_3.panorama_hotspots.append(panorama_hotspot_3_1)
+
+# --- tour
+
+tour = Tour(
+    title="Tour exemple",
+    first_panorama=panorama_2,
+    panoramas=[panorama_1, panorama_2, panorama_3],
+    nadir=photo_nadir,
+)
